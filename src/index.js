473fe7e3c35c7f6443d34b98156ad313f5a5f766
minhman <phminhman2501@gmail.com>
2022-10-10 20:52:16 +0700

    change UserContext comp name

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-import { UserContext } from './layout/UserContext';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { UserProvider } from "./layout/UserContext";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <UserContext>
+    <UserProvider>
       <App />
-    </UserContext>
+    </UserProvider>
   </BrowserRouter>
 );
 
--- src/layout/UserContext.jsx
+++ src/layout/UserContext.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
 export const userContext = React.createContext();
-const localUser = localStorage.getItem('user');
+const localUser = localStorage.getItem("user");
 
-export const UserContext = ({ children }) => {
+export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(localUser ? JSON.parse(localUser) : {});
 
   const globalUser = {
