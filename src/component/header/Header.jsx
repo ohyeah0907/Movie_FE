9fbbfd260c51f02906212a1806af5cd674608946
minhman <phminhman2501@gmail.com>
2022-10-15 00:35:52 +0700

    Header with logo and onClick event

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -2,15 +2,24 @@ import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import { routes } from "../../routes/routes";
 import { userContext } from "../../layout/UserContext";
+import styles from "./Header.module.scss";
+import clsx from "clsx";
+import { MainLogo } from "../Logo";
 
 export const Header = () => {
   const [navHeader, setNavHeader] = useState(routes);
+  const [active, setActive] = useState("Home");
   const user = useContext(userContext);
 
   const handleSignOut = () => {
     user.handleUser({});
     localStorage.clear();
     reverseHide();
+  };
+
+  const handleActive = (text) => {
+    console.log(text);
+    setActive(text);
   };
 
   // reverse property hide of object
@@ -24,32 +33,76 @@ export const Header = () => {
       });
     });
   };
-
   useEffect(() => {
     if (user.name) reverseHide();
   }, [user]);
 
   return (
-    <div >
-      <div>{user.name ? user.name : ""}</div>
-      {navHeader.map((route) => {
-        if (route.hasOwnProperty("hide")) {
-          if (route.hide === false) {
-            return (
-              <Link to={route.path} key={route.text}>
-                {route.text}
-              </Link>
-            );
-          }
-        } else
-          return (
-            <Link to={route.path} key={route.text}>
-              {route.text}
-            </Link>
-          );
-        return null;
-      })}
-      {user.name ? <button onClick={handleSignOut}>Sign out</button> : ""}
+    <div className={styles.navbar}>
+      <div
+        className={clsx(
+          "container-md align-items-center justify-content-betwwen ",
+          styles.container
+        )}
+      >
+        <Link
+          className={clsx(
+            "d-flex align-items-center link",
+            styles.navbar__logo
+          )}
+        >
+          <MainLogo />
+          <h1 className={clsx(styles.navbar__text)}>
+            <span className="bold">Rest</span> and{" "}
+            <span className="bold">Relax</span>
+          </h1>
+        </Link>
+        <ul className={clsx("align-items-center", styles.navbar__nav)}>
+          {user.name ? <div>{user.name}</div> : ""}
+          {navHeader.map((route) => {
+            if (route.hasOwnProperty("hide")) {
+              if (route.hide === false) {
+                return (
+                  <li className={styles.navItem} key={route.text}>
+                    <Link
+                      className={clsx(
+                        "link",
+                        styles.navItem__link,
+                        route.text === active
+                          ? styles["navItem__link--active"]
+                          : ""
+                      )}
+                      to={route.path}
+                      onClick={() => handleActive(route.text)}
+                    >
+                      {route.text}
+                    </Link>
+                  </li>
+                );
+              }
+            } else
+              return (
+                <li className={styles.navItem} key={route.text}>
+                  <Link
+                    className={clsx(
+                      "link",
+                      styles.navItem__link,
+                      route.text === active
+                        ? styles["navItem__link--active"]
+                        : ""
+                    )}
+                    to={route.path}
+                    onClick={() => handleActive(route.text)}
+                  >
+                    {route.text}
+                  </Link>
+                </li>
+              );
+            return null;
+          })}
+          {user.name ? <button onClick={handleSignOut}>Sign out</button> : ""}
+        </ul>
+      </div>
     </div>
   );
 };
