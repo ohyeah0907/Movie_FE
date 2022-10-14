eb0002d51c6ee5281aa887b5fd25aae67e76614b
minhman <phminhman2501@gmail.com>
2022-10-15 00:44:40 +0700

    change MainLogo using memo

--- src/component/Logo/MainLogo.js
+++ src/component/Logo/MainLogo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { memo } from "react";
 import clsx from "clsx";
 
 const MainLogo = ({ width, height }) => {
@@ -65,4 +65,4 @@ const MainLogo = ({ width, height }) => {
   );
 };
 
-export default MainLogo;
+export default memo(MainLogo);
