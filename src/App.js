653f905258256178a54d9a43f4312d17523073fe
minhman <phminhman2501@gmail.com>
2022-10-14 23:43:47 +0700

    Revert "Header with logo and onClick event"
    This reverts commit 7584e6080794398dbfac32b9ae76a1734442aa13.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import "./App.css";
-import React from "react";
-import { Default } from "./layout/Default";
-import { AdminPage } from "./page/admin-page/AdminPage";
-import GlobalStyles from "./component/GlobalStyles";
-import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import React from 'react';
+import { Default } from './layout/Default';
+import { AdminPage } from './page/admin-page/AdminPage';
 function App() {
   // Test component
   return (
-    <GlobalStyles>
-      <div className="App">
-        <Default />
-        {/* <AdminPage /> */}
-      </div>
-    </GlobalStyles>
+    <div className="App">
+      <Default />
+      {/* <AdminPage /> */}
+    </div>
   );
 }
 
