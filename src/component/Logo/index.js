11db7374e3408a1776dd976cb0eeda0dc31aaf3f
minhman <phminhman2501@gmail.com>
2022-10-15 11:42:33 +0700

    add burger icon

--- src/component/Logo/index.js
+++ src/component/Logo/index.js
@@ -1,1 +1,2 @@
 export { default as MainLogo } from "./MainLogo";
+export { default as Burger } from "./burger";
--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -4,7 +4,7 @@ import { routes } from "../../routes/routes";
 import { userContext } from "../../layout/UserContext";
 import styles from "./Header.module.scss";
 import clsx from "clsx";
-import { MainLogo } from "../Logo";
+import { MainLogo, Burger } from "../Logo";
 
 export const Header = () => {
   const [navHeader, setNavHeader] = useState(routes);
@@ -46,7 +46,7 @@ export const Header = () => {
     <div className={styles.navbar}>
       <div
         className={clsx(
-          "container-lg align-items-center justify-content-betwwen ",
+          "container-lg  px-0 align-items-center justify-content-betwwen ",
           styles.container
         )}
       >
@@ -55,6 +55,7 @@ export const Header = () => {
             "d-flex align-items-center link",
             styles.navbar__logo
           )}
+          to="/"
         >
           <MainLogo />
           <h1 className={clsx(styles.navbar__text)}>
@@ -63,16 +64,16 @@ export const Header = () => {
           </h1>
         </Link>
         <div
-          onClick={() => {
-            handleShow();
-          }}
           className={clsx(
             "d-xl-none d-lg-none d-md-block text-white z",
             styles.burger__btn,
             showSideBar ? styles["burger__btn--over"] : ""
           )}
+          onClick={() => {
+            handleShow();
+          }}
         >
-          Burger
+          <Burger size="30" fill="#fff"></Burger>
         </div>
         <ul className={clsx("align-items-center", styles.navbar__nav)}>
           {user.name ? <div>{user.name}</div> : ""}
