f6f3df28e33b1bf02f222704243614d7ef6bad24
minhman <phminhman2501@gmail.com>
2022-10-29 12:37:16 +0700

    update

--- src/page/category-page/CategoryPage.jsx
+++ src/page/category-page/CategoryPage.jsx
@@ -1,136 +1,10 @@
 import clsx from "clsx";
 import styles from "./css/CategoryPage.module.scss";
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Action } from "@remix-run/router";
 import { Item } from "../../component/item/Item";
-
-const movieList = [
-  {
-    name: "The Batman",
-    poster:
-      "https://i.pinimg.com/736x/83/13/30/831330643120469f94d9adaba542b516.jpg",
-    backdrop:
-      "https://bloody-disgusting.com/wp-content/uploads/2022/01/the-batman-new-poster-2.png",
-    rating: 70,
-    star: 49,
-    genres: ["Action", "Detective"],
-    overview:
-      "Batman ventures into Gotham City's underworld when a sadistic killer leaves behind a trail of cryptic clues. As the evidence begins to lead closer to home and the scale of the perpetrator's plans become clear, he must forge new relationships, unmask the culprit and bring justice to the abuse of power and corruption that has long plagued the metropolis.",
-    id: "1",
-  },
-  {
-    name: "Avengers: End Game",
-    poster:
-      "https://hips.hearstapps.com/hmg-prod/images/d1pklzbuyaab0la-1552597012.jpg",
-    backdrop: "https://cdn.mos.cms.futurecdn.net/QFuzEB2Gxth6qQT8Tpe7LN.jpg",
-    rating: 75,
-    star: 73,
-    genres: ["Action", "Sci-Fi"],
-    overview: "overview 2",
-    id: "2",
-  },
-  {
-    name: "Avengers: Infinity War",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/vi/e/e8/Avengers-Infinity_War-Official-Poster.jpg",
-    backdrop:
-      "https://waghostwriter.com/wp-content/uploads/2018/05/landscape-1522924460-avengers-infinity-war-poster.jpg",
-    rating: 90,
-    star: 72,
-    genres: ["Action", "Sci-Fi"],
-    overview: "overview 3",
-    id: "3",
-  },
-  {
-    name: "Avengers: Infinity War",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/vi/e/e8/Avengers-Infinity_War-Official-Poster.jpg",
-    backdrop:
-      "https://waghostwriter.com/wp-content/uploads/2018/05/landscape-1522924460-avengers-infinity-war-poster.jpg",
-    rating: 90,
-    star: 72,
-    genres: ["Action", "Sci-Fi"],
-    overview: "overview 3",
-    id: "3",
-  },
-  {
-    name: "Avengers: Infinity War",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/vi/e/e8/Avengers-Infinity_War-Official-Poster.jpg",
-    backdrop:
-      "https://waghostwriter.com/wp-content/uploads/2018/05/landscape-1522924460-avengers-infinity-war-poster.jpg",
-    rating: 90,
-    star: 72,
-    genres: ["Action", "Sci-Fi"],
-    overview: "overview 3",
-    id: "3",
-  },
-  {
-    name: "Avengers: Infinity War",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/vi/e/e8/Avengers-Infinity_War-Official-Poster.jpg",
-    backdrop:
-      "https://waghostwriter.com/wp-content/uploads/2018/05/landscape-1522924460-avengers-infinity-war-poster.jpg",
-    rating: 90,
-    star: 72,
-    genres: ["Action", "Sci-Fi"],
-    overview: "overview 3",
-    id: "3",
-  },
-
-  {
-    name: "Avengers: Infinity War",
-    poster:
-      "https://upload.wikimedia.org/wikipedia/vi/e/e8/Avengers-Infinity_War-Official-Poster.jpg",
-    backdrop:
-      "https://waghostwriter.com/wp-content/uploads/2018/05/landscape-1522924460-avengers-infinity-war-poster.jpg",
-    rating: 90,
-    star: 72,
-    genres: ["Action", "Sci-Fi"],
-    overview: "overview 3",
-    id: "3",
-  },
-];
-
-const categoryList = [
-  {
-    id: 1,
-    name: "Action",
-  },
-  {
-    id: 2,
-    name: "Anime",
-  },
-  {
-    id: 3,
-    name: "Dramas",
-  },
-  {
-    id: 4,
-    name: "Romance",
-  },
-  {
-    id: 5,
-    name: "Sus",
-  },
-  {
-    id: 6,
-    name: "Sussy",
-  },
-  {
-    id: 7,
-    name: "LMAO",
-  },
-  {
-    id: 6,
-    name: "Sussy",
-  },
-  {
-    id: 7,
-    name: "LMAO",
-  },
-];
+import { movieList, categoryList } from "../../component/testdata";
 
 export const CategoryPage = () => {
   const featureList = movieList.filter((movie, index) => {
@@ -139,7 +13,9 @@ export const CategoryPage = () => {
 
   const renderMenuColumns = (menuList = [], maxRow) => {
     var menuColumn = [];
-    while (menuList.length > 0) menuColumn.push(menuList.splice(0, maxRow));
+    for (let index = 0; index < menuList.length; index += maxRow + 1) {
+      menuColumn.push(menuList.slice(index, maxRow + index));
+    }
     return menuColumn;
   };
 
