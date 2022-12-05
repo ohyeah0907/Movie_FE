0df58b243d8666a334e4d2024c9e5c856bd4758d
minhman <phminhman2501@gmail.com>
2022-12-05 21:48:04 +0700

    getMovie and getGenres services

---
+++ src/service/component/movie-genres/index.js
@@ -0,0 +1,12 @@
+import { instance } from "../../../constant/axios/Axios";
+export const getAllGenres = async (signal) => {
+  return await instance
+    .get("/genres", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
