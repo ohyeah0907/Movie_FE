00b9649af6007a2889dbf87d0952940fd3e88119
minhman <phminhman2501@gmail.com>
2022-12-08 23:38:51 +0700

    create: list page base UI

--- src/page/list-page/ListPage.jsx
+++ src/page/list-page/ListPage.jsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import clsx from "clsx";
+import styles from "./ListPage.module.scss";
+import { DefaultSlider as Slider } from "../../component/slider/Default";
+import { NormalItem as Item } from "../../component/item";
+import { getAllMovie } from "../../service/component/movie";
+import { Row, Col } from "react-bootstrap";
+import { useState } from "react";
 
 export const ListPage = () => {
-  return <div>ListPage</div>;
+  const [userMovies, setUserMovies] = useState([]);
+  const controller = new AbortController();
+
+  useEffect(() => {
+    let getData = async () => {
+      let data = await getAllMovie(controller.signal);
+      let dataFiltered = data.filter(
+        (item) => item.title !== null && item.overview.length > 0
+      );
+      setUserMovies(dataFiltered);
+    };
+    getData().catch(console.error);
+  }, []);
+
+  return (
+    <div className={clsx("section")}>
+      <div className="container">
+        <div className={clsx(styles["section__heading"])}>
+          <div className={clsx(styles["section__title"])}>
+            My Favourite Movie List
+          </div>
+          <div className={clsx(styles["section__paragraph"])}>
+            Here are your beloved movies.
+          </div>
+        </div>
+        <div className="row gx-3 gy-4">
+          {userMovies.map((movie) => (
+            <Col md={2} sm={3}>
+              <Item movie={movie} layout="normal-home"></Item>
+            </Col>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
