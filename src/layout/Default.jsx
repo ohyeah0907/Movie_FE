24368a1ded1ac980e8ed43e6b15400a16aaf8602
minhman <phminhman2501@gmail.com>
2022-12-08 22:41:55 +0700

    update: delete breadcrump

--- src/layout/Default.jsx
+++ src/layout/Default.jsx
@@ -1,14 +1,13 @@
-import React from 'react';
-import { Header } from '../component/header/Header';
-import { Footer } from '../component/footer/Footer';
-import { Main } from '../component/main/Main';
-import { BreadCrumb } from '../component/breadcrumb/BreadCrumb';
+import React from "react";
+import { Header } from "../component/header/Header";
+import { Footer } from "../component/footer/Footer";
+import { Main } from "../component/main/Main";
+import { BreadCrumb } from "../component/breadcrumb/BreadCrumb";
 
 export const Default = () => {
   return (
     <>
       <Header />
-      <BreadCrumb />
       <Main />
       <Footer />
     </>
