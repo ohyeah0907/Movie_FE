0b932bb56fc617518628b680e2b72e90617ee987
minhman <phminhman2501@gmail.com>
2022-12-09 11:13:03 +0700

    update: tv show slider

--- src/page/category-page/CategoryPage.jsx
+++ src/page/category-page/CategoryPage.jsx
@@ -1,9 +1,9 @@
-import clsx from 'clsx';
-import styles from './css/CategoryPage.module.scss';
-import React, { useEffect, useRef, useState } from 'react';
-import { DefaultSlider } from '../../component/slider/Default';
-import { getAllGenres } from '../../service/component/movie-genres';
-import { useLocation } from 'react-router-dom';
+import clsx from "clsx";
+import styles from "./css/CategoryPage.module.scss";
+import React, { useEffect, useRef, useState } from "react";
+import { DefaultSlider } from "../../component/slider/Default";
+import { getAllGenres } from "../../service/component/movie-genres";
+import { useLocation } from "react-router-dom";
 
 export const CategoryPage = () => {
   const location = useLocation();
@@ -12,7 +12,7 @@ export const CategoryPage = () => {
   const data = location.state?.selectedGenre;
 
   const [genresMenu, setGenresMenu] = useState([]);
-  const [currentGenre, setCurrentGenre] = useState('Genres');
+  const [currentGenre, setCurrentGenre] = useState("Genres");
   const [showCategoryMenu, setShowCatergoryMenu] = useState(false);
 
   const handleChangeGenre = (selectedGenre) => {
@@ -48,63 +48,63 @@ export const CategoryPage = () => {
   }, [data]);
 
   useEffect(() => {
-    document.querySelector('body').scrollTo(0, 0);
+    document.querySelector("body").scrollTo(0, 0);
   }, []);
 
   return (
     <div className={clsx(styles.category)}>
-      <div className={clsx(styles['category-nav'])}>
+      <div className={clsx(styles["category-nav"])}>
         <div
           className={clsx(
-            'container d-flex align-items-center',
-            styles['category__wrapper']
+            "container d-flex align-items-center",
+            styles["category__wrapper"]
           )}
         >
           <div
-            className={clsx('d-flex align-items-center', [
-              styles['category-nav__wrapper'],
-              showCategoryMenu ? styles['category-nav__wrapper--focus'] : '',
+            className={clsx("d-flex align-items-center", [
+              styles["category-nav__wrapper"],
+              showCategoryMenu ? styles["category-nav__wrapper--focus"] : "",
             ])}
           >
-            {currentGenre !== 'Genres' && (
+            {currentGenre !== "Genres" && (
               <span
                 className={clsx(
-                  'd-inline-flex justify-content-between align-items-center',
-                  styles['category-nav__button'],
-                  styles['category-nav__button--label']
+                  "d-inline-flex justify-content-between align-items-center",
+                  styles["category-nav__button"],
+                  styles["category-nav__button--label"]
                 )}
-                onClick={() => setCurrentGenre('Genres')}
+                onClick={() => setCurrentGenre("Genres")}
               >
                 Genres <i className="fa-solid fa-angle-right"></i>
               </span>
             )}
             <div
               className={clsx(
-                'd-inline-flex justify-content-between align-items-center',
-                styles['category-nav__button'],
-                currentGenre !== 'Genres'
-                  ? styles['category-nav__button--selected']
-                  : ''
+                "d-inline-flex justify-content-between align-items-center",
+                styles["category-nav__button"],
+                currentGenre !== "Genres"
+                  ? styles["category-nav__button--selected"]
+                  : ""
               )}
               onClick={() => {
                 setShowCatergoryMenu(!showCategoryMenu);
               }}
             >
               <div>
-                <span className={styles['category-nav__button-text']}>
+                <span className={styles["category-nav__button-text"]}>
                   <i
-                    className={clsx('fa-sharp fa-solid fa-film', styles.icon)}
+                    className={clsx("fa-sharp fa-solid fa-film", styles.icon)}
                   ></i>
                   {currentGenre}
                 </span>
               </div>
-              <i className={clsx('fa-solid fa-caret-down')}></i>
+              <i className={clsx("fa-solid fa-caret-down")}></i>
             </div>
             <div
-              className={clsx('d-none row mx-0', styles['category-nav__menu'])}
+              className={clsx("d-none row mx-0", styles["category-nav__menu"])}
             >
-              <div className={styles['category-nav__menu-close']}>
-                <span className={styles['category-nav__menu-close-btn']}>
+              <div className={styles["category-nav__menu-close"]}>
+                <span className={styles["category-nav__menu-close-btn"]}>
                   Exit
                 </span>
               </div>
@@ -112,12 +112,12 @@ export const CategoryPage = () => {
                 genresMenu.map((menuCol, index) => (
                   <div
                     key={index}
-                    className={clsx('col', styles['category-nav__menu-col'])}
+                    className={clsx("col", styles["category-nav__menu-col"])}
                   >
                     {menuCol.map((menuLink) => (
                       <div
                         key={menuLink.id}
-                        className={clsx(styles['category-nav__menu-link'])}
+                        className={clsx(styles["category-nav__menu-link"])}
                         onClick={() => handleChangeGenre(menuLink.name)}
                       >
                         {menuLink.name}
@@ -131,28 +131,38 @@ export const CategoryPage = () => {
       </div>
       <div className="content-section">
         <div
-          className={clsx('container', styles.section, styles['movie-feature'])}
+          className={clsx("container", styles.section, styles["movie-feature"])}
         >
-          <div className={clsx(styles['movie-feature__wrapper'])}>
+          <div className={clsx(styles["movie-feature__wrapper"])}>
             <div
               className={clsx(
-                styles['movie-title'],
-                styles['movie-title--feature']
+                styles["movie-title"],
+                styles["movie-title--feature"]
               )}
             >
               Featuring Movies
             </div>
             <DefaultSlider
               layout="feature"
-              category={currentGenre !== 'Genres' ? currentGenre : null}
+              category={currentGenre !== "Genres" ? currentGenre : null}
             />
           </div>
-          <div className={clsx(styles['movie-default'])}>
+          <div className={clsx(styles["movie-default"])}>
             <div className={clsx(styles.section)}>
-              <div className={clsx(styles['movie-title'])}>Movies</div>
+              <div className={clsx(styles["movie-title"])}>Movies</div>
               <DefaultSlider
                 layout="normal"
-                category={currentGenre !== 'Genres' ? currentGenre : null}
+                category={currentGenre !== "Genres" ? currentGenre : null}
+              />
+            </div>
+          </div>
+          <div className={clsx(styles["movie-default"])}>
+            <div className={clsx(styles.section)}>
+              <div className={clsx(styles["movie-title"])}>TV Show</div>
+              <DefaultSlider
+                layout="normal"
+                category={currentGenre !== "Genres" ? currentGenre : null}
+                tvShow={true}
               />
             </div>
           </div>
