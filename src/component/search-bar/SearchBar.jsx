88a6b210758150def6ea03e5242f824b782c4ccd
minhman <phminhman2501@gmail.com>
2022-12-14 14:55:15 +0700

    final: CSS Search Bar

--- src/component/search-bar/SearchBar.jsx
+++ src/component/search-bar/SearchBar.jsx
@@ -1,40 +1,40 @@
-import clsx from 'clsx';
-import React, { useState } from 'react';
-import styles from './css/searchBar.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { icon } from '@fortawesome/fontawesome-svg-core/import.macro';
-import { useNavigate } from 'react-router-dom';
+import clsx from "clsx";
+import React, { useState } from "react";
+import styles from "./css/searchBar.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { useNavigate } from "react-router-dom";
 
 export const SearchBar = () => {
   const navigate = useNavigate();
 
   const getFilmBySearch = (e) => {
-    if (e.key == 'Enter') {
+    if (e.key == "Enter") {
       navigate(`/search?name=${e.currentTarget.value}`);
-      e.currentTarget.value = '';
+      e.currentTarget.value = "";
     }
   };
   return (
-    <div className={clsx(styles['search-bar'])}>
-      <FontAwesomeIcon
-        className={clsx(
-          styles['search-bar__icon'],
-          styles['search-bar__icon--search']
-        )}
-        icon={icon({ name: 'magnifying-glass', style: 'solid' })}
-      />
+    <div className={clsx(styles["search-bar"])}>
       <input
-        className={clsx(styles['search-bar__input-content'])}
+        className={clsx(styles["search-bar__input-content"])}
         onKeyUp={(e) => {
           getFilmBySearch(e);
         }}
       />
       <FontAwesomeIcon
         className={clsx(
-          styles['search-bar__icon'],
-          styles['search-bar__icon--delete']
+          styles["search-bar__icon"],
+          styles["search-bar__icon--search"]
         )}
-        icon={icon({ name: 'xmark', style: 'solid' })}
+        icon={icon({ name: "magnifying-glass", style: "solid" })}
+      />
+      <FontAwesomeIcon
+        className={clsx(
+          styles["search-bar__icon"],
+          styles["search-bar__icon--delete"]
+        )}
+        icon={icon({ name: "xmark", style: "solid" })}
       />
     </div>
   );
