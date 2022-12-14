7444314e0bad2aadcb22f029bcd36afc9b02cfad
TriPham <phamductri2001@gmail.com>
2022-12-14 14:22:36 +0700

    add header when show admin page

--- src/layout/Default.jsx
+++ src/layout/Default.jsx
@@ -3,20 +3,11 @@ import { Header } from '../component/header/Header';
 import { Footer } from '../component/footer/Footer';
 import { Main } from '../component/main/Main';
 // import { BreadCrumb } from "../component/breadcrumb/BreadCrumb";
-import { decodeToken } from 'react-jwt';
-import { userContext } from './UserContext';
 
 export const Default = () => {
-  const context = useContext(userContext);
-  console.log('Default');
-  const currentUser = decodeToken(localStorage.getItem('access_token'));
   return (
     <>
-      {!currentUser ? (
-        <Header />
-      ) : (
-        currentUser.sub == 'admin@gmail.com' || <Header />
-      )}
+      <Header />
       <Main />
       <Footer />
     </>
