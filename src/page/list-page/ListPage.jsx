454053df5b225081fbc7ca909009efc64ba304cb
minhman <phminhman2501@gmail.com>
2022-12-14 15:10:46 +0700

    update: List Page css

--- src/page/list-page/ListPage.jsx
+++ src/page/list-page/ListPage.jsx
@@ -1,18 +1,18 @@
-import React, { useEffect } from 'react';
-import clsx from 'clsx';
-import styles from './ListPage.module.scss';
-import { DefaultSlider as Slider } from '../../component/slider/Default';
-import { NormalItem as Item } from '../../component/item';
-import { getAllMovie } from '../../service/component/movie';
-import { Row, Col } from 'react-bootstrap';
-import { useState } from 'react';
-import { decodeToken } from 'react-jwt';
-import { getUserDetail } from '../../service/component/userWishList';
+import React, { useEffect } from "react";
+import clsx from "clsx";
+import styles from "./ListPage.module.scss";
+import { DefaultSlider as Slider } from "../../component/slider/Default";
+import { NormalItem as Item } from "../../component/item";
+import { getAllMovie } from "../../service/component/movie";
+import { Row, Col } from "react-bootstrap";
+import { useState } from "react";
+import { decodeToken } from "react-jwt";
+import { getUserDetail } from "../../service/component/userWishList";
 
 export const ListPage = () => {
   const [userMovies, setUserMovies] = useState([]);
   const controller = new AbortController();
-  const token = localStorage.getItem('access_token');
+  const token = localStorage.getItem("access_token");
 
   useEffect(() => {
     let getUserWishList = async () => {
@@ -31,20 +31,20 @@ export const ListPage = () => {
   }, []);
 
   return (
-    <div className={clsx('section')}>
+    <div className={clsx("section")}>
       <div className="container">
-        <div className={clsx(styles['section__heading'])}>
-          <div className={clsx(styles['section__title'])}>
-            My Favourite Movie List{' '}
+        <div className={clsx(styles["section__heading"])}>
+          <div className={clsx(styles["section__title"])}>
+            My Favourite Movie List{" "}
           </div>
-          <div className={clsx(styles['section__paragraph'])}>
+          <div className={clsx(styles["section__paragraph"])}>
             Here are your beloved movies.
           </div>
         </div>
-        <div className="row gx-3 gy-4">
+        <div className="row gx-2 gy-4">
           {userMovies.map((movie) => (
             <Col md={2} sm={3}>
-              <Item movie={movie} layout="normal-home"></Item>
+              <Item movie={movie} layout="feature"></Item>
             </Col>
           ))}
         </div>
